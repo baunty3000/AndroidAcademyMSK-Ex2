f0fc3093237a882bd5e7ec87ffc4671506d42e5a
SergeyM <malakhovj@gmail.com>
2018-11-05 00:27:16 +0300

    Учел все замечания ментора! Полностью сделал задание №4 по потокам.

--- NY Times/src/main/java/ru/malakhov/nytimes/data/Category.java
+++ NY Times/src/main/java/ru/malakhov/nytimes/data/Category.java
@@ -3,29 +3,53 @@ package ru.malakhov.nytimes.data;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.util.Objects;
+
 public class Category implements Parcelable {
-    private final int id;
-    private final String name;
+    private final int mId;
+    private final String mName;
 
     public Category(int id, String name) {
-        this.id = id;
-        this.name = name;
+        mId = id;
+        mName = name;
     }
 
     protected Category(Parcel in) {
-        id = in.readInt();
-        name = in.readString();
+        mId = in.readInt();
+        mName = in.readString();
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public String getName() {
+        return mName;
     }
 
     @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(id);
-        dest.writeString(name);
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Category category = (Category) o;
+        return mId == category.mId && Objects.equals(mName, category.mName);
     }
 
     @Override
-    public int describeContents() {
-        return 0;
+    public int hashCode() {
+        return Objects.hash(mId, mName);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{"
+                + "mId=" + mId
+                + ", mName='" + mName + '\''
+                + '}';
     }
 
     public static final Creator<Category> CREATOR = new Creator<Category>() {
@@ -40,11 +64,14 @@ public class Category implements Parcelable {
         }
     };
 
-    public int getId() {
-        return id;
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(mId);
+        dest.writeString(mName);
     }
 }
--- NY Times/src/main/java/ru/malakhov/nytimes/data/NewsItem.java
+++ NY Times/src/main/java/ru/malakhov/nytimes/data/NewsItem.java
@@ -3,47 +3,87 @@ package ru.malakhov.nytimes.data;
 import android.os.Parcel;
 import android.os.Parcelable;
 import java.util.Date;
+import java.util.Objects;
 
 public class NewsItem implements Parcelable {
 
-    private final String title;
-    private final String imageUrl;
-    private final Category category;
-    private final Date publishDate;
-    private final String previewText;
-    private final String fullText;
+    private final String mTitle;
+    private final String mImageUrl;
+    private final Category mCategory;
+    private final Date mPublishDate;
+    private final String mPreviewText;
+    private final String mFullText;
 
-    public NewsItem(String title, String imageUrl, Category category, Date publishDate, String previewText, String fullText) {
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.category = category;
-        this.publishDate = publishDate;
-        this.previewText = previewText;
-        this.fullText = fullText;
+    public NewsItem(String title, String imageUrl, Category category, Date publishDate,
+            String previewText, String fullText) {
+        mTitle = title;
+        mImageUrl = imageUrl;
+        mCategory = category;
+        mPublishDate = publishDate;
+        mPreviewText = previewText;
+        mFullText = fullText;
     }
 
     protected NewsItem(Parcel in) {
-        title = in.readString();
-        imageUrl = in.readString();
-        category = in.readParcelable(Category.class.getClassLoader());
-        previewText = in.readString();
-        fullText = in.readString();
-        publishDate = new Date(in.readLong());
+        mTitle = in.readString();
+        mImageUrl = in.readString();
+        mCategory = in.readParcelable(Category.class.getClassLoader());
+        mPreviewText = in.readString();
+        mFullText = in.readString();
+        mPublishDate = new Date(in.readLong());
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+    public Category getCategory() {
+        return mCategory;
+    }
+    public Date getPublishDate() {
+        return mPublishDate;
+    }
+    public String getPreviewText() {
+        return mPreviewText;
+    }
+    public String getFullText() {
+        return mFullText;
     }
 
     @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(title);
-        dest.writeString(imageUrl);
-        dest.writeParcelable(category, flags);
-        dest.writeString(previewText);
-        dest.writeString(fullText);
-        dest.writeLong(publishDate.getTime());
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        NewsItem newsItem = (NewsItem) o;
+        return Objects.equals(mTitle, newsItem.mTitle) && Objects
+                .equals(mImageUrl, newsItem.mImageUrl) && Objects
+                .equals(mCategory, newsItem.mCategory) && Objects
+                .equals(mPublishDate, newsItem.mPublishDate) && Objects
+                .equals(mPreviewText, newsItem.mPreviewText) && Objects
+                .equals(mFullText, newsItem.mFullText);
     }
 
     @Override
-    public int describeContents() {
-        return 0;
+    public int hashCode() {
+        return Objects.hash(mTitle, mImageUrl, mCategory, mPublishDate, mPreviewText, mFullText);
+    }
+
+    @Override
+    public String toString() {
+        return "NewsItem{"
+                + "mTitle='" + mTitle + '\''
+                + ", mImageUrl='" + mImageUrl + '\''
+                + ", mCategory=" + mCategory
+                + ", mPublishDate=" + mPublishDate
+                + ", mPreviewText='" + mPreviewText + '\''
+                + ", mFullText='" + mFullText + '\''
+                + '}';
     }
 
     public static final Creator<NewsItem> CREATOR = new Creator<NewsItem>() {
@@ -58,22 +98,18 @@ public class NewsItem implements Parcelable {
         }
     };
 
-    public String getTitle() {
-        return title;
+    @Override
+    public int describeContents() {
+        return 0;
     }
-    public String getImageUrl() {
-        return imageUrl;
-    }
-    public Category getCategory() {
-        return category;
-    }
-    public Date getPublishDate() {
-        return publishDate;
-    }
-    public String getPreviewText() {
-        return previewText;
-    }
-    public String getFullText() {
-        return fullText;
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mTitle);
+        dest.writeString(mImageUrl);
+        dest.writeParcelable(mCategory, flags);
+        dest.writeString(mPreviewText);
+        dest.writeString(mFullText);
+        dest.writeLong(mPublishDate.getTime());
     }
 }
